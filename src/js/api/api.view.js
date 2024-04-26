3024a216e1d404c16304c4624283ceb7ea032bab
Akbar <akbarjonjorayev001@gmail.com>
2024-04-26 16:19:09 +0500

    app created

---
+++ src/js/api/api.view.js
@@ -0,0 +1,11 @@
+export const API = {
+  firebase: {
+    apiKey: 'https://firebase.google.com',
+    authDomain: 'https://firebase.google.com',
+    projectId: 'https://firebase.google.com',
+    storageBucket: 'https://firebase.google.com',
+    messagingSenderId: 'https://firebase.google.com',
+    appId: 'https://firebase.google.com',
+    measurementId: 'https://firebase.google.com',
+  },
+}
