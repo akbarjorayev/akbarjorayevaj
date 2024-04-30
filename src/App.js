9414dbb615ede16e0ee6c9c6335e9df5b787ded0
Akbar <akbarjonjorayev001@gmail.com>
2024-04-30 14:55:38 +0500

    naming

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
 
   useEffect(() => {
     async function loadData() {
-      let data = await load('me')
+      const data = await load('me')
       setAccount(data)
     }
     loadData()
