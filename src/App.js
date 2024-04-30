1893cbbfead1170f6a316c9c51c3d2fa2bb95c3c
Akbar <akbarjonjorayev001@gmail.com>
2024-04-30 14:53:25 +0500

    fName & lName added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import { useEffect, useState } from 'react'
+
 import Avatar from './components/Avatar/Avatar'
+
+import { load } from './js/db/db'
 
 import './css/App.css'
 
 export default function App() {
+  const [account, setAccount] = useState({})
+
+  useEffect(() => {
+    async function loadData() {
+      let data = await load('me')
+      setAccount(data)
+    }
+    loadData()
+  }, [])
+
   return (
     <>
-      <Avatar />
+      <Avatar img={account.img?.img} />
+      <div>
+        <span>{account.fName}</span>
+        <span> </span>
+        <span>{account.lName}</span>
+      </div>
     </>
   )
 }
