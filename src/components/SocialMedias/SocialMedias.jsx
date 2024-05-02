01671e1b9d10d8e388adfbb78be00bbadd76be9b
Akbar <akbarjonjorayev001@gmail.com>
2024-05-02 11:21:33 +0500

    new -> SocialMedias.jsx

---
+++ src/components/SocialMedias/SocialMedias.jsx
@@ -0,0 +1,15 @@
+export default function SocialMedias() {
+  return (
+    <>
+      <a href="https://github.com/akbarjorayev" rel="noreferrer">
+        GitHub
+      </a>
+      <a href="https://t.me/akbarjorayevAJ" rel="noreferrer">
+        Telegram
+      </a>
+      <a href="https://www.linkedin.com/in/akbarjorayevaj" rel="noreferrer">
+        Linkedin
+      </a>
+    </>
+  )
+}
